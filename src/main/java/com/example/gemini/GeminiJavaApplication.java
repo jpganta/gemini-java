06010795bd009342406921d39796974aa5bd7f01
Jayaprakash Ganta <jganta@care.com>
2025-04-11 09:33:23 -0400

    created initial spring project

---
+++ src/main/java/com/example/gemini/GeminiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiJavaApplication.class, args);
+	}
+
+}
